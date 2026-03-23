b04c651a443aa6d0a390386a502c06a43c1c68e3
surya <suryajamwal@143.com>
2026-03-23 10:49:22 +0530

    UC11 Updated

--- README.md
+++
@@ -1,1 +0,0 @@
-"# Quantity_Measurement_App" 
---
+++ UC11/src/main/java/org/apps/quantitymeasurement/LengthUnitUC11.java
@@ -0,0 +1,27 @@
+package org.apps.quantitymeasurement;
+
+public enum LengthUnitUC11 implements IMeasurableUC11 {
+
+    FEET(12.0),
+    INCHES(1.0),
+    YARDS(36.0),
+    CENTIMETERS(0.393701);
+
+    private final double conversionFactor;
+
+    LengthUnitUC11(double conversionFactor) {
+        this.conversionFactor = conversionFactor;
+    }
+
+    public double getConversionFactor() {
+        return conversionFactor;
+    }
+
+    public double convertToBaseUnit(double value) {
+        return value * conversionFactor;
+    }
+
+    public double convertFromBaseUnit(double baseValue) {
+        return baseValue / conversionFactor;
+    }
+}
